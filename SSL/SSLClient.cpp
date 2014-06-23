5e9c3ed22d95c6d6b10767bf2d7ae95fc2f3e867
balika011 <balika011@gmail.com>
2014-06-23 18:02:27 +0200

    Clean up

--- SSL/SSLClient.cpp
+++ SSL/SSLClient.cpp
@@ -5,27 +5,6 @@
 
 #include "SSLClient.h"
 
-/*void ShowCerts(SSL* ssl)
-{
-	X509 *cert;
-	char *line;
-
-	cert = SSL_get_peer_certificate(ssl);
-	if (cert != NULL)
-	{
-		printf("Server certificates:\n");
-		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
-		printf("Subject: %s\n", line);
-		free(line);
-		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
-		printf("Issuer: %s\n", line);
-		free(line)
-		X509_free(cert);
-	}
-	else
-		printf("No certificates.\n");
-}*/
-
 SSLClient::SSLClient(const char* hostname, WORD port)
 {
 	struct hostent *host = gethostbyname(hostname);
